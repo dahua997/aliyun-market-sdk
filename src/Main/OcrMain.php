d66bd583715432d63db5210348735cb852b4f027
dahua997 <97277929+dahua997@users.noreply.github.com>
2022-04-07 06:57:23 +0800

    优化代码

--- src/Main/OcrMain.php
+++ src/Main/OcrMain.php
@@ -13,6 +13,8 @@ class OcrMain
 {
     private $c;
     private $header;
+    private $url;
+    private $body;
 
     public function __construct()
     {
@@ -25,6 +27,12 @@ class OcrMain
         return $this;
     }
 
+    public function url($url)
+    {
+        $this->url = $url;
+        return $this;
+    }
+
     public function body($body)
     {
         $this->body  = $body;
@@ -34,9 +42,13 @@ class OcrMain
     public function post()
     {
         $response = $this->c->setData([
+            '_url'=>$this->url,
             '_data'=>$this->body,
             '_header'=>$this->header
         ])->post();
-        return $response['_res']['response'];
+        if(!empty($response->_res['response'])){
+            return json_decode($response->_res['response'], true);
+        }
+        return [];
     }
 }
